440d1d83699e2e3f393e6f327147c36d57037f1d
bcat <v01z@mail.ru>
2023-02-14 22:28:23 +0300

    Подготовлен перевод проекта на CMake

--- EditorTeam/helpbrowser.h
+++ EditorTeam/helpbrowser.h
@@ -1,39 +1,15 @@
-#include <QtWidgets>
-#include <QApplication>
+#ifndef HELPBROWSER_H
+#define HELPBROWSER_H
 
-class HelpBrowser : public QWidget
-{
-    Q_OBJECT
+#include <QApplication>
+#include <QtWidgets>
+
+class HelpBrowser : public QWidget {
+  Q_OBJECT
 
 public:
-    HelpBrowser(const QString &strPath,
-                const QString &strFileName,
-                QWidget *pwgt = 0) : QWidget(pwgt)
-    {
-    QPushButton *pressBack = new QPushButton("Back",this);
-    QPushButton *pressForward = new QPushButton("Forward", this);
-    QPushButton *pressHome = new QPushButton("Home",this);
-    QTextBrowser *txtBrowser = new QTextBrowser(this);
-
-
-    connect(pressBack, SIGNAL(clicked(bool)), txtBrowser, SLOT(backward()));
-    connect(pressForward, SIGNAL(clicked(bool)), txtBrowser, SLOT(forward()));
-    connect(pressHome, SIGNAL(clicked(bool)), txtBrowser, SLOT(home()));
-    connect(txtBrowser, SIGNAL(backwardAvailable(bool)), pressBack, SLOT(setEnabled(bool)));
-    connect(txtBrowser, SIGNAL(forwardAvailable(bool)), pressForward, SLOT(setEnabled(bool)));
-
-    txtBrowser->setSearchPaths(QStringList() << strPath);
-    txtBrowser->setSource(QString(strFileName));
-
-    QVBoxLayout *vboxLayout = new QVBoxLayout(this);
-    QHBoxLayout *hboxLayout = new QHBoxLayout(this);
-
-    hboxLayout -> addWidget(pressBack);
-    hboxLayout -> addWidget(pressForward);
-    hboxLayout -> addWidget(pressHome);
-    vboxLayout -> addLayout(hboxLayout);
-    vboxLayout -> addWidget(txtBrowser);
-    setLayout(vboxLayout);
-    }
-
+  HelpBrowser(const QString &strPath, const QString &strFileName,
+              QWidget *pwgt = nullptr);
 };
+
+#endif // HELPBROWSER_H
