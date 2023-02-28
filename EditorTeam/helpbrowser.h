362ca5cc1a72eb70a73227885f27b41a94ceb1ad
AJIKAIII <78610228+AJIKAIIIMMM@users.noreply.github.com>
2023-02-28 22:53:03 +0300

    Revert "R2T14_64[Dev] Реализовал функцию `Crossed` согласно ТТ (#83)"
    This reverts commit 3ff84fb236a675e8d29d66506f1e79794d6388a7.

--- EditorTeam/helpbrowser.h
+++ EditorTeam/helpbrowser.h
@@ -1,15 +1,39 @@
-#ifndef HELPBROWSER_H
-#define HELPBROWSER_H
-
-#include <QApplication>
 #include <QtWidgets>
+#include <QApplication>
 
-class HelpBrowser : public QWidget {
-  Q_OBJECT
+class HelpBrowser : public QWidget
+{
+    Q_OBJECT
 
 public:
-  HelpBrowser(const QString &strPath, const QString &strFileName,
-              QWidget *pwgt = 0);
-};
+    HelpBrowser(const QString &strPath,
+                const QString &strFileName,
+                QWidget *pwgt = 0) : QWidget(pwgt)
+    {
+    QPushButton *pressBack = new QPushButton("Back",this);
+    QPushButton *pressForward = new QPushButton("Forward", this);
+    QPushButton *pressHome = new QPushButton("Home",this);
+    QTextBrowser *txtBrowser = new QTextBrowser(this);
 
-#endif // HELPBROWSER_H
+
+    connect(pressBack, SIGNAL(clicked(bool)), txtBrowser, SLOT(backward()));
+    connect(pressForward, SIGNAL(clicked(bool)), txtBrowser, SLOT(forward()));
+    connect(pressHome, SIGNAL(clicked(bool)), txtBrowser, SLOT(home()));
+    connect(txtBrowser, SIGNAL(backwardAvailable(bool)), pressBack, SLOT(setEnabled(bool)));
+    connect(txtBrowser, SIGNAL(forwardAvailable(bool)), pressForward, SLOT(setEnabled(bool)));
+
+    txtBrowser->setSearchPaths(QStringList() << strPath);
+    txtBrowser->setSource(QString(strFileName));
+
+    QVBoxLayout *vboxLayout = new QVBoxLayout(this);
+    QHBoxLayout *hboxLayout = new QHBoxLayout(this);
+
+    hboxLayout -> addWidget(pressBack);
+    hboxLayout -> addWidget(pressForward);
+    hboxLayout -> addWidget(pressHome);
+    vboxLayout -> addLayout(hboxLayout);
+    vboxLayout -> addWidget(txtBrowser);
+    setLayout(vboxLayout);
+    }
+
+};
