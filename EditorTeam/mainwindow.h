9d7d9a015cbc00643c76d62a5fde9ad0e1ab707a
AJIKAIII <78610228+AJIKAIIIMMM@users.noreply.github.com>
2023-02-28 22:53:37 +0300

    Revert "R2T14_64[Dev] Реализовал функцию `Crossed` согласно ТТ (#83)" (#89)
    This reverts commit 3ff84fb236a675e8d29d66506f1e79794d6388a7.

--- EditorTeam/mainwindow.h
+++ EditorTeam/mainwindow.h
@@ -1,76 +1,67 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include "helpbrowser.h"
-#include "filehandler.h"
+#include <QFile>
 #include <QMainWindow>
-#include <QSharedPointer>
+#include <QFile>
 #include <QTextEdit>
 #include <QTranslator>
+#include <QSharedPointer>
+#include "helpbrowser.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui
-{
+namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
-  public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+public:
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
-  private:
-    Ui::MainWindow *ui;
+private:
+  Ui::MainWindow *ui;
 
-  /* Флаг "Содержимое textEdit изменено?" */
-  bool isTextModified;
+  /*! GubaydullinRG
+  Флаг "Содержимое textEdit изменено?" */
+  bool isModified;
+  //Указатель на текущий редактируемый файл
+  QFile *file;
 
-  /*! GubaydullinRG В textEdit загружен новый документ? */
-  bool newDataLoaded;
+  QSharedPointer<HelpBrowser> hb;
 
-  // Указатель на текущий редактируемый файл
-  QSharedPointer<IDevHandler<QString>> srcHandler;
+  /*! KuznecovAG
+    Переменная для текущего стиля (пока только white и grey) */
+  QString currentStyle = "white";
 
-    QSharedPointer<HelpBrowser> hb;
-
-    /*! KuznecovAG
-      Переменная для текущего стиля (пока только white и grey) */
-    QString currentStyle = "white";
-    // Тулбар главной панели
-    QToolBar *mainToolBar;
-
-  // Пункты меню
+  //Пункты меню
   QMenu *fileMenu;
   QMenu *editMenu;
-  QMenu *formatMenu;
   QMenu *settingsMenu;
   QMenu *questionMenu;
 
   // Вспомогательные методы для выноса части
   // кода за пределы конструктора
-  void createAction(QAction **, const QString &, void (MainWindow::*)());
+  void createAction(QAction**, void (MainWindow::*)());
   void createActions();
   void createMenus();
 
-  // Интернационализация приложения
+  //Интернационализация приложения
   QTranslator *translator;
-  virtual void changeEvent(QEvent *) override;
-  void retranslateAction(QAction **, const QPair<const char *, const char *> &);
+  virtual void changeEvent(QEvent*) override;
+  void retranslateAction(QAction**, const QPair<const char*, const char*>&);
   void retranslateActions();
   void retranslateMenus();
   void retranslateGUI();
 
-  /*! GubaydullinRG
-      Включение/отключение доступности элементов меню 'File' */
-  void changeFileMenuAccess(const QString &, bool, bool, bool);
+  bool warningWindow(); // Окно предупреждения
+  void changeEnableActions(); // Переключатель кнопки Close
 
-  bool textChangedWarning(); // Окно предупреждения
-
-  // Элементы подменю 'File'
+  //Элементы подменю 'File'
   QAction *newAction;
   QAction *openAction;
   QAction *closeAction;
@@ -79,7 +70,7 @@ class MainWindow : public QMainWindow
   QAction *printAction;
   QAction *exitAction;
 
-  // Элементы подменю 'Edit'
+  //Элементы подменю 'Edit'
   QAction *copyTextFormatAction;
   QAction *applyTextFormatAction;
   QAction *alignTextRightAction;
@@ -87,23 +78,23 @@ class MainWindow : public QMainWindow
   QAction *alignTextCenterAction;
   QAction *switchFontAction;
 
-  // Элементы подменю 'Format'
-      QAction *crossedTextFormatAction;
-
-  // Элементы подменю 'Settings'
+  //Элементы подменю 'Settings'
   QAction *changeLangAction;
   QAction *changeKeyBindAction;
   QAction *changeStyleAction;
 
-  // Поле для размещения редактируемого текста
+  //Поле для размещения редактируемого текста
   QTextEdit *textEdit;
+  QString lastFilename;
 
-  // Элементы подменю '?'
+  bool isTextModified = false;
+    
+  //Элементы подменю '?'
   QAction *helpAction;
   QAction *aboutAction;
 
 private slots:
-  // Основные функции приложения
+  //Основные функции приложения
   void onNew();
   void onOpen();
   void onClose();
@@ -122,13 +113,10 @@ private slots:
   void onChangeStyle();
   void onHelp();
   void onAbout();
-  void onCrossedTextFormat();
 
-    /*! GubaydullinRG
-    // Слот действий на случай изменения
-    // содержимого textEdit */
-    void onTextModified();
-
-    void setMainToolBar();
+  /*! GubaydullinRG
+  // Слот действий на случай изменения
+  // содержимого textEdit */
+  void onTextModified();
 };
 #endif // MAINWINDOW_H
