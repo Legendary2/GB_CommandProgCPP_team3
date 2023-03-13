47e2d7bea42fdd08ea1aa8bbe100ed0d147b1516
flagrans <flagrans@mail.ru>
2023-03-13 23:58:03 +0700

    Разработка функционала согласто ТЗ

---
+++ EditorTeam/searchhighlight.h
@@ -0,0 +1,29 @@
+#ifndef SEARCHHIGHLIGHT_H
+#define SEARCHHIGHLIGHT_H
+
+#include <QRegularExpression>
+#include <QSyntaxHighlighter>
+
+class SearchHighLight : public QSyntaxHighlighter
+{
+    Q_OBJECT
+    using BaseClass = QSyntaxHighlighter;
+
+  public:
+    explicit SearchHighLight(QTextDocument *parent = nullptr);
+    ~SearchHighLight();
+
+    void searchText(const QString &, bool isСaseInsensitive = false);
+    void clearText();
+    int coutResult();
+
+  protected:
+    virtual void highlightBlock(const QString &text) override;
+
+  private:
+    QRegularExpression patternHighLight;
+    QTextCharFormat formatHighLight;
+    int count;
+};
+
+#endif // SEARCHHIGHLIGHT_H
